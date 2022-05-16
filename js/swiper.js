f0af6a604513447d04d8736fef58112e7a2cd74e
Andrey Seragov <cristalix6@gmail.com>
2022-05-16 12:36:43 +0300

    Добавление адаптивности

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,5 @@
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
-  slidesPerView: 3,
   spaceBetween: 0,
   loop: true,
   centeredSlides: true,
@@ -13,5 +12,19 @@ const swiper = new Swiper('.swiper', {
   scrollbar: {
     el: '.swiper-scrollbar',
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      
+    },
+    1000: {
+      slidesPerView: 2,
+      
+    },
+    1920: {
+      slidesPerView: 3,
+      
+    },
+  }
 
 });
