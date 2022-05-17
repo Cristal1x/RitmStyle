40cc4c654c528f68b317650562781320d18db646
Andrey Seragov <cristalix6@gmail.com>
2022-05-17 10:45:35 +0300

    Ломает бургер дальше

--- js/main.js
+++ js/main.js
@@ -3,21 +3,21 @@ const nav = document.querySelector('.header__nav');
 const header = document.querySelector('.header');
 const logo = document.querySelector('.header__logo');
 const button = document.querySelector('.header__button');
+const image = document.querySelector('.header__image');
 
 burger.addEventListener('click', function() {
-  console.log(nav.className);
   if (nav.classList.contains('show')) {
     header.classList.remove('column');
     nav.classList.add('hide');
     nav.classList.remove('show');
-    burger.style.backgroundImage = "url('../img/hamburger.png')";
+    image.src = "../img/hamburger.png";
     logo.classList.remove('hide');
     button.classList.remove('hide');
   } else {
     nav.classList.add('show');
     nav.classList.remove('hide');
     header.classList.add('column');
-    burger.style.backgroundImage = "url('../img/close.png')";
+    image.src = "../img/close.png";
     logo.classList.add('hide');
     button.classList.add('hide');
   }
